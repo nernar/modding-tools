de70c3724eeadc6a2bc16c7d6e70e93a57ba8ebf
MaXFeeD <maksfeed.mf@gmail.com>
2025-12-02 02:48:02 +0300

    Updated Sequence library

--- declarations/Sequence.d.ts
+++ declarations/Sequence.d.ts
@@ -1,81 +1,93 @@
+/**
+ * @constructor
+ * Processes thread besides [[sync()]] interface
+ * context, that was used to indicate process.
+ * Calls [[process()]] for every element requested
+ * by [[next()]] besides [[execute()]] passed value.
+ * @param {object} [obj] merges with prototype
+ */
+declare function Sequence(obj?: object): void;
 declare class Sequence {
-    static uid: number;
-    id: string;
-    reporting?: boolean;
-    index?: number;
-    count?: number;
-    between?: number;
-    updated?: boolean;
-    completed?: boolean;
-    priority?: number;
-    thread?: java.lang.Thread;
-    update?: (progress: number, index: number) => void;
-    tick?: (index: number, ellapsedMs: number, startMs: number) => void;
-    complete?: (ellapsedMs: number, startMs: number) => void;
-    uncount?: (value: any) => number;
-    create?: (value: any, startMs: number) => void;
     /**
+     * @constructor
      * Processes thread besides [[sync()]] interface
      * context, that was used to indicate process.
      * Calls [[process()]] for every element requested
      * by [[next()]] besides [[execute()]] passed value.
-     * @param obj merges with prototype
+     * @param {object} [obj] merges with prototype
      */
-    constructor(obj?: Scriptable);
-    getThread(): java.lang.Thread;
+    constructor(obj?: object);
+    update: any;
+    tick: any;
+    complete: any;
+    uncount: any;
+    create: any;
+    completed: any;
+    id: string;
+    getThread(): {};
     getSynchronizeTime(): number;
-    setSynchronizeTime(ms: number): void;
+    setSynchronizeTime(ms: any): void;
+    between: number;
     getPriority(): number;
-    setPriority(priority: number): void;
-    setFixedCount(count: number): void;
+    setPriority(priority: any): void;
+    priority: number;
+    setFixedCount(count: any): void;
+    count: number;
     getFixedCount(): number;
-    setReportingEnabled(enabled: boolean): void;
+    setReportingEnabled(enabled: any): void;
+    reporting: boolean;
     isReportingEnabled(): boolean;
     /**
      * Sync recursive action, that awaits when
      * process is completed, interrupted or cancelled.
-     * @param active process startup milliseconds
+     * @param {number} active process startup milliseconds
      */
     sync(active: number): void;
     /**
      * Action that launches main process and sync.
-     * @param value data to process
+     * @param {any} [value] data to process
      */
     execute(value?: any): void;
+    index: number;
+    thread: {};
     /**
      * Must be called inside [[process()]] or [[next()]]
      * if you want to force update process indexes.
      * Recommended to use if [[uncount()]] wouldn't
      * help in dynamical reupdate or just update progress.
-     * @param index currently progress
-     * @param count maximum value
+     * @param {number} [index] currently progress
+     * @param {number} [count] maximum value
      */
     require(index?: number, count?: number): void;
-    shrink(addition?: number): void;
+    updated: boolean;
+    shrink(addition: any): void;
     /**
+     * @async Wouldn't access interface thread.
      * Calls for every item inside [[process]], passed
      * value will be used into it. That action created
      * to communicate executing object with process,
      * split it to processable parts.
-     * @param value passed on execute
-     * @param index was returned by [[process()]]
-     * @returns value or element to [[process()]]
-     * @async Wouldn't access interface thread.
+     * @param {any} value passed on execute
+     * @param {number} index was returned by [[process()]]
+     * @returns {any} value or element to [[process()]]
      */
     next(value: any, index: number): any;
     /**
+     * @async Wouldn't access interface thread.
      * Main sequence process in thread;
      * handles object and returns index.
-     * @param element next result to handle
-     * @param value elements resolver
-     * @param index indicates progress
-     * @returns index to sync
+     * @param {any} element next result to handle
+     * @param {any} value elements resolver
+     * @param {number} index indicates progress
+     * @returns {number} index to sync
      * @throws must be overwritten in usage
-     * @async Wouldn't access interface thread.
      */
     process(element: any, value: any, index: number): number;
-    cancel(error?: Error): void;
+    cancel(error: any): void;
     interrupt(): void;
-    isInterrupted(): boolean;
-    assureYield(thread?: java.lang.Thread): boolean;
+    isInterrupted(): any;
+    assureYield(thread: any): boolean;
+}
+declare namespace Sequence {
+    let uid: number;
 }
